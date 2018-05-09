c19f74eaf28c03871602f4ba71446822133065cd
Daur Gamisonia <daurgam@gmail.com>
2018-05-09 08:43:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Unofficial Google+ app for macOS
+
+Independent site-wrapper application for macOS which created specially for Google+ social network and have a notification badge for unread messages. The application is simply builded with [Nativefier](https://github.com/jiahaog/nativefier) but contains my script for notification counter.
+
+![Screenshot](https://github.com/BroFox86/google-plus-wrapper-app/blob/master/screenshot.png)
+
+# Installation
+
+* Download latest build from the [releases](https://github.com/BroFox86/google-plus-wrapper-app/releases) page.
+* Copy *Google+.app* to */Application* folder.
---
+++ listener.js
@@ -0,0 +1,27 @@
+/* ==========================================================================
+  Created by Daur "BroFox" Gamisonia.
+  Google Plus: https://plus.google.com/+DaurGamisoniya
+  Email: daurgam@gmail.com
+  ========================================================================== */
+
+var targetNode = document.getElementsByClassName("gb_b")[1],
+        config = { attributes: true };
+
+var callback = function(mutationsList) {
+  for (var mutation of mutationsList) {
+    if (mutation.attributeName == "title") {
+
+      var amount = targetNode.getAttribute("title").match(/[1-99]$/),               appName = "Google+ ";
+
+      if (amount != null) {
+        document.title = appName + "(" + amount + ")";
+      } else {
+        document.title = appName;
+      }
+    }
+  }
+};
+
+var observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
