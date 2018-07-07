6dc85eb1ac0679364ad5c7984ad13666f15a7a8a
Daur Gamisonia <daurgam@gmail.com>
2018-07-07 23:16:25 +0300

    Refactor the notification script

--- notification.js
+++ notification.js
@@ -2,28 +2,23 @@
   Notification counter listener for Google Plus.
   Designed to use with Nativefier on macOS (github.com/jiahaog/nativefier).
 
-  Created by Daur "BroFox" Gamisonia.
-  github.com/BroFox86
+  Created by Daur "BroFox" Gamisonia (github.com/BroFox86).
 
   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   Example of use:
-  
+
   nativefier "plus.google.com" --counter --inject path/to/notification.js --icon path/to/icon.png
   ========================================================================== */
 
 // Select the node that will be observed for mutations
 var targetNode = document.getElementsByClassName("gb_b")[1];
 
-// Options for the observer (which mutations to observe)
-var config = { attributes: true };
-
 // Name of the application
 var appName = "Google+ ";
 
-// Callback function to execute when mutations are observed
-var callback = function (mutationsList) {
-  for (var mutation of mutationsList) {
-
+// Create the MutationObserver
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
     // Get a number of unread notifications
     var amount = targetNode.getAttribute("title").match(/[1-99]$/);
 
@@ -34,11 +29,11 @@ var callback = function (mutationsList) {
       // Or leave the application name alone
       document.title = appName;
     }
-  }
-};
-
-// Create an observer instance linked to the callback function
-var observer = new MutationObserver(callback);
-
+  });
+});
+ 
+// Options for the observer (which mutations to observe)
+var config = { attributes: true};
+ 
 // Start observing the target node for configured mutations
 observer.observe(targetNode, config);
